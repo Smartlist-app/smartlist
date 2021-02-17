373bf6d9d069ec8bd5bbcff43aa644d48e97839c
ManuTheCoder <77016441+ManuTheCoder@users.noreply.github.com>
2021-02-16 18:05:05 -0800

    Update locales.js

--- extension/locales.js
+++ extension/locales.js
@@ -1,10 +1,10 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 const kLocales = {
-'https://smartlist.ga/dashboard/rooms/garage/add.php': 'Add to Garage',
-'https://smartlist.ga/dashboard/rooms/garage/add.php': 'Add to Garage',
-'https://pramodini.github.io/Homebase/Index.htm': 'Add this to my inventory in Homebase',
-'https://pramodini.github.io/Homebase/beta.htm': 'Add this to my inventory in Homebase beta',
-'https://homebase-dev-app.slack.com': 'Add this to my inventory in Homebase Slack'};
+'https://smartlist.ga/dashboard/rooms/add.php': 'Add to Kitchen',
+'https://smartlist.ga/dashboard/rooms/bathroom/add.html': 'Add to Bathroom',
+'https://smartlist.ga/dashboard/rooms/bedroom/addx.php': 'Add to Bedroom',
+'https://smartlist.ga/dashboard/rooms/family/add.html': 'Add to Family (Living) room',
+'https://smartlist.ga/dashboard/rooms/storage/add.html': 'Add to Storage room',
+'https://smartlist.ga/dashboard/rooms/dining_room/add.html': 'Add to Dining Room',
+'https://smartlist.ga/dashboard/rooms/camping/add.html': 'Add to Camping Supplies',
+'https://smartlist.ga/dashboard/rooms/laundry/add.html': 'Add to Laundry room',
+};
