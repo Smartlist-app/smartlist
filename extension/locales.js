eb126ce58cf1dc75cdfac5b76f8090f48692c17b
ManuTheCoder <77016441+ManuTheCoder@users.noreply.github.com>
2021-02-16 17:57:51 -0800

    Add files via upload

---
+++ extension/locales.js
@@ -0,0 +1,10 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const kLocales = {
+'https://smartlist.ga/dashboard/rooms/garage/add.php': 'Add to Garage',
+'https://smartlist.ga/dashboard/rooms/garage/add.php': 'Add to Garage',
+'https://pramodini.github.io/Homebase/Index.htm': 'Add this to my inventory in Homebase',
+'https://pramodini.github.io/Homebase/beta.htm': 'Add this to my inventory in Homebase beta',
+'https://homebase-dev-app.slack.com': 'Add this to my inventory in Homebase Slack'};
